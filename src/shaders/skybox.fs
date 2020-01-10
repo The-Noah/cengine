90a3b03bff6e15af4a042eb5903083977d9fb93c
The Noah <thenoahbz@gmail.com>
2020-01-09 20:12:40 -0600

    Improve skybox shader

--- src/shaders/skybox.fs
+++ src/shaders/skybox.fs
@@ -5,5 +5,8 @@
 "out vec4 FragColor;\n"
 
 "void main(){\n"
-"  gl_FragColor = vec4(mix(vec3(0.0, 0.1, 0.2), vec3(0.3, 0.6, 0.8), vCoord.y / 50.0 + 0.5), 1.0);\n"
+"  vec3 sky_normal = normalize(vCoord.xyz);\n"
+"  float gradient = dot(sky_normal, vec3(0.0, 1.0, 0.0) * 0.5 /*+ 0.5*/);\n"
+"  gl_FragColor = vec4(vec3(gradient) + vec3(0.3, 0.6, 0.8), 1.0);\n"
+// "  gl_FragColor = vec4(mix(vec3(0.0, 0.1, 0.2), vec3(0.3, 0.6, 0.8), vCoord.y / 50.0 + 0.5), 1.0);\n"
 "}";
