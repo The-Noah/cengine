cd2e3f4883c42249c21d2159476d3a50c1d5e5e3
The Noah <thenoahbz@gmail.com>
2020-01-10 15:19:36 -0600

    Show shader errors

--- src/renderer/shader.c
+++ src/renderer/shader.c
@@ -6,15 +6,43 @@
 #include <GL\glew.h>
 
 unsigned int shader_create(const char* vertex_source, const char* fragment_source){
+  int success;
+
   unsigned int vertex_shader;
   vertex_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertex_shader, 1, &vertex_source, NULL);
   glCompileShader(vertex_shader);
 
+  // check compile errors
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if(!success){
+    char infoLog[512];
+    glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+    printf("vertex shader error\n");
+    printf(infoLog);
+    printf("\n");
+
+    glDeleteShader(vertex_shader);
+    return 0;
+  }
+
   unsigned int fragment_shader;
   fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragment_shader, 1, &fragment_source, NULL);
   glCompileShader(fragment_shader);
+
+  // check compile errors
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+  if(!success){
+    char infoLog[512];
+    glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+    printf("fragment shader error\n");
+    printf(infoLog);
+    printf("\n");
+
+    glDeleteShader(fragment_shader);
+    return 0;
+  }
 
   unsigned int shader_program;
   shader_program = glCreateProgram();
@@ -24,6 +52,19 @@ unsigned int shader_create(const char* vertex_source, const char* fragment_sourc
   glLinkProgram(shader_program);
   glDeleteShader(vertex_shader);
   glDeleteShader(fragment_shader);
+
+  // check compile errors
+  glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+  if(!success){
+    char infoLog[512];
+    glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+    printf("shader program error\n");
+    printf(infoLog);
+    printf("\n");
+
+    glDeleteShader(fragment_shader);
+    return 0;
+  }
 
   return shader_program;
 }
