e11c5b38412f45490e1ede0e3bfa136cb2368b9a
The Noah <thenoahbz@gmail.com>
2020-01-27 21:56:36 -0600

    Change voxel color

--- src/shaders/voxel.fs
+++ src/shaders/voxel.fs
@@ -3,5 +3,5 @@
 "in vec4 vTexCoord;\n"
 
 "void main(){\n"
-"  gl_FragColor = vec4(vTexCoord.w / 128.0, vTexCoord.w / 256.0, vTexCoord.w / 512.0, 1.0);\n"
+"  gl_FragColor = vec4(0.0, 0.3, 0.05, 1.0);\n"
 "}"
