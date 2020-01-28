5cf6e0435217f66476a21bf3230f761a30820c9b
The Noah <thenoahbz@gmail.com>
2020-01-28 17:17:49 -0600

    Add basic fog

--- src/shaders/voxel.fs
+++ src/shaders/voxel.fs
@@ -6,5 +6,8 @@
 "uniform sampler2D diffuse_texture;\n"
 
 "void main(){\n"
-"  gl_FragColor = vec4(texture(diffuse_texture, vTexCoord).rgb * vBrightness, 1.0);\n"
+"  vec3 color = texture(diffuse_texture, vTexCoord).rgb * vBrightness;\n"
+"  float f = pow(clamp(gl_FragCoord.z / gl_FragCoord.w / 128, 0, 1), 2);\n"
+
+"  gl_FragColor = vec4(mix(color, vec3(0.53, 0.81, 0.92), f), 1.0);\n"
 "}"
