78740561e78c91e88a9cf58d6100569e372125b7
The Noah <thenoahbz@gmail.com>
2020-02-07 12:13:52 -0600

    Add useful commands to premake script

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,38 @@
 workspace "cengine"
   configurations {"Debug", "Release"}
 
+  newaction {
+    ["trigger"] = "clean",
+    ["description"] = "Delete generated project and build files",
+    ["onStart"] =
+      function()
+	      print "Cleaning files..."
+      end,
+    ["execute"] =
+      function()
+        if _TARGET_OS == "windows" then
+          os.execute("IF EXIST bin ( RMDIR /S /Q bin )")
+          os.execute("IF EXIST obj ( RMDIR /S /Q obj )")
+          os.execute("del Makefile *.make")
+        else
+          os.execute("rm -rf bin obj Makefile *.make")
+        end
+      end,
+    ["onEnd"] =
+      function()
+	      print "Done."
+      end
+  }
+
+  newaction {
+    ["trigger"] = "dev",
+    ["description"] = "Build and run",
+    ["execute"] =
+      function()
+        os.execute("premake5 gmake2 && mingw32-make && bin\\Debug\\cengine.exe")
+      end
+  }
+
 project "cengine"
   language "C"
   targetdir "bin/%{cfg.buildcfg}" 
