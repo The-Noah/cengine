2e624dac7139cae1d70977ca1d8650f1af95f115
The Noah <thenoahbz@gmail.com>
2020-07-28 18:03:35 -0500

    Move tiny C code into headers

--- src/renderer/utils.h
+++ src/renderer/utils.h
@@ -4,6 +4,14 @@
 #define GLEW_STATIC
 #include "GL/glew.h"
 
-unsigned int make_buffer(GLenum target, GLsizei size, const void* data);
+unsigned int make_buffer(GLenum target, GLsizei size, const void* data){
+  unsigned int buffer;
+
+  glGenBuffers(1, &buffer);
+  glBindBuffer(target, buffer);
+  glBufferData(target, size, data, GL_STATIC_DRAW);
+
+  return buffer;
+}
 
 #endif
